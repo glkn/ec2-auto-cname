2ae596e19638792b7f4b969f3eb1a365e5d2298f
Alexey Galkin <agalkin@whitebox.ru>
2015-07-09 22:27:29 +0300

    ec2 public hostname retreival added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var aws = require('aws-sdk'),
     route53,
     _ = require('lodash'),
+    request = request('request'),
     argv = require('minimist')(process.argv.slice(2));
 
 aws.config.update({
@@ -9,38 +10,53 @@ aws.config.update({
     region: argv.region
 });
 
-route53 = new aws.Route53();
-
-route53.listHostedZones({}, function(err, data) {
-    if (!err) {
-        var domain = _.find(data.HostedZones, {
-            Name: argv.domain
-        });
-        if ( !domain ) {
-            console.log('Domain not found:', argv.domain);
-            return;
+if (!argv.publicHostName) {
+    request('http://169.254.169.254/latest/meta-data/public-hostname', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('Public hostname:', body);
+            argv.publicHostName = body;
+        } else {
+            console.log(error);
         }
-        var params = {
-            "HostedZoneId": domain.Id,
-            "ChangeBatch": {
-                "Changes": [{
-                    "Action": "UPSERT",
-                    "ResourceRecordSet": {
-                        "Name": argv.resourceName,
-                        "Type": "CNAME",
-                        "TTL": 60,
-                        "ResourceRecords": [{
-                            "Value": argv.publicDNS
-                        }]
-                    }
-                }]
-            }
-        };
+    });
+} else {
+    changeRecord();
+}
 
-        route53.changeResourceRecordSets(params, function(err, data) {
-            console.log(err, data);
-        });
-    } else {
-        console.error(err);
-    }
-});
+function changeRecord() {
+    route53 = new aws.Route53();
+
+    route53.listHostedZones({}, function(err, data) {
+        if (!err) {
+            var domain = _.find(data.HostedZones, {
+                Name: argv.domain
+            });
+            if (!domain) {
+                console.log('Domain not found:', argv.domain);
+                return;
+            }
+            var params = {
+                "HostedZoneId": domain.Id,
+                "ChangeBatch": {
+                    "Changes": [{
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": argv.resourceName,
+                            "Type": "CNAME",
+                            "TTL": 60,
+                            "ResourceRecords": [{
+                                "Value": argv.publicHostName
+                            }]
+                        }
+                    }]
+                }
+            };
+
+            route53.changeResourceRecordSets(params, function(err, data) {
+                console.log(err, data);
+            });
+        } else {
+            console.error(err);
+        }
+    });
+}
